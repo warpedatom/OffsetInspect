c795a8bfb6131118e5ebb8e1610623d2fc7b8ba4
Velkris <160296292+warpedatom@users.noreply.github.com>
2025-12-30 02:34:43 -0500

    Update OffsetInspect.psm1

--- module/OffsetInspect.psm1
+++ module/OffsetInspect.psm1
@@ -15,25 +15,29 @@
 function Invoke-OffsetInspect {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory=$true)]
-        [string]$FilePath,
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string[]]$FilePaths,
 
-        [Parameter(Mandatory=$true)]
-        [string]$OffsetInput,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string[]]$OffsetInputs,
 
         [int]$ByteWindow = 32,
         [int]$ContextLines = 3
     )
 
     # Resolve tool path relative to module directory
-    $scriptPath = Join-Path $PSScriptRoot "..\OffsetInspect.ps1"
+    $scriptPath = Join-Path -Path $PSScriptRoot -ChildPath "..\OffsetInspect.ps1"
 
-    if (-not (Test-Path $scriptPath)) {
+    if (-not (Test-Path -LiteralPath $scriptPath)) {
         throw "OffsetInspect.ps1 not found at expected location: $scriptPath"
     }
 
     # Invoke the actual script with parameters
-    & $scriptPath -FilePath $FilePath -OffsetInput $OffsetInput -ByteWindow $ByteWindow -ContextLines $ContextLines
+    & $scriptPath `
+        -FilePaths    $FilePaths `
+        -OffsetInputs $OffsetInputs `
+        -ByteWindow   $ByteWindow `
+        -ContextLines $ContextLines
 }
 
 Export-ModuleMember -Function Invoke-OffsetInspect
